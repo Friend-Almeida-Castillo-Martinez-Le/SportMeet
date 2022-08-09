9c0c2e5a6f0a06a9ee8a0eddccc65de51e7dd6ab
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-08-09 12:13:14 -0500

    first push

---
+++ src/main/java/com/codeup/sportmeet/SportMeetApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.sportmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportMeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportMeetApplication.class, args);
+    }
+
+}
