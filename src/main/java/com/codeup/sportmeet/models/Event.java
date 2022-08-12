a855a55f2122aafb8fd7998a7d07cc11538b9399
Michael Friend <michael.l.friend2@gmail.com>
2022-08-12 14:02:54 -0500

    event model update

--- src/main/java/com/codeup/sportmeet/models/Event.java
+++ src/main/java/com/codeup/sportmeet/models/Event.java
@@ -1,4 +1,19 @@
 package com.codeup.sportmeet.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "events")
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
 }
