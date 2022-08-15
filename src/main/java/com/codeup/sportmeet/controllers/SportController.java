c11076a7de83618d11ae991958d9c2bd81858dda
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-08-15 10:43:53 -0500

    update3 with events and sports lists

--- src/main/java/com/codeup/sportmeet/controllers/SportController.java
+++ src/main/java/com/codeup/sportmeet/controllers/SportController.java
@@ -2,6 +2,8 @@ package com.codeup.sportmeet.controllers;
 
 import com.codeup.sportmeet.repositories.SportRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SportController {
@@ -10,5 +12,11 @@ public class SportController {
 
     public SportController(SportRepository sportDao) {
         this.sportDao = sportDao;
+    }
+
+    @GetMapping("/sports")
+    public String displaySports(Model model){
+        model.addAttribute("sports", sportDao.findAll());
+        return "/sport/index";
     }
 }
