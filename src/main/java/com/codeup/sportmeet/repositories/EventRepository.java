8bdc72df49211b8a6b995bc3e40bbe45c7222f2b
Michael Friend <michael.l.friend2@gmail.com>
2022-08-15 10:59:25 -0500

    friend-branch2

--- src/main/java/com/codeup/sportmeet/repositories/EventRepository.java
+++ src/main/java/com/codeup/sportmeet/repositories/EventRepository.java
@@ -2,7 +2,17 @@ package com.codeup.sportmeet.repositories;
 
 
 import com.codeup.sportmeet.models.Event;
+import com.codeup.sportmeet.models.Sport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+//    @Transactional
+//    @Modifying
+//    @Query("update Event event set event.title = :title, event.description = :description, event.date = :date, event.startTime = :startTime, event.endTime = :endTIme, event.location = :location, event.sport = :sport where event.id = :id")
+//    void updateEvent(@Param("id") long id, @Param("title") String title, @Param("description") String description, @Param("date") long date, @Param("startTime") String startTime, @Param("endTime") String endTime, @Param("location") String location, @Param("sport")Sport sport);
 }
