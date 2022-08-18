a239f2751f2d6da53ff7d3da3dde1bd8818ce796
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-08-18 10:44:55 -0500

    comments

--- seeder_events.sql
+++ seeder_events.sql
@@ -1,2 +1,12 @@
 use readyup_db;
 
+
+insert into sports (name)
+values ('Tennis'),
+       ('Basketball'),
+       ('Football'),
+       ('Soccer'),
+       ('Baseball'),
+       ('Golf'),
+       ('Bowling'),
+       ('Volleyball');
