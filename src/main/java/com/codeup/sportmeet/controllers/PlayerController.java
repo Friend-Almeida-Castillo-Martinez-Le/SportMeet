bed991d8b694668957647dcbffd0fc2f0b6e0f67
Michael Friend <michael.l.friend2@gmail.com>
2022-08-19 12:20:46 -0500

    updated

--- src/main/java/com/codeup/sportmeet/controllers/PlayerController.java
+++ src/main/java/com/codeup/sportmeet/controllers/PlayerController.java
@@ -30,13 +30,13 @@ public class PlayerController {
         return "/player/index";
     }
 
-    @GetMapping("player/sign-up")
+    @GetMapping("/sign-up")
     public String showCreateForm(Model model) {
         model.addAttribute("player", new Player());
         return "/player/sign-up";
     }
 
-    @PostMapping("player/sign-up")
+    @PostMapping("/sign-up")
     public String playerCreate(@ModelAttribute Player player) {
         String hash = passwordEncoder.encode(player.getPassword());
         player.setPassword(hash);
