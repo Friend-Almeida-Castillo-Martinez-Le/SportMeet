433cee9321b787dd36aecbc47ede92479a627506
Michael Friend <michael.l.friend2@gmail.com>
2022-08-24 11:31:53 -0500

    comment deletion

--- src/main/java/com/codeup/sportmeet/controllers/CommentController.java
+++ src/main/java/com/codeup/sportmeet/controllers/CommentController.java
@@ -5,6 +5,7 @@ import com.codeup.sportmeet.models.Event;
 import com.codeup.sportmeet.models.Player;
 import com.codeup.sportmeet.repositories.CommentRepository;
 import com.codeup.sportmeet.repositories.EventRepository;
+import com.codeup.sportmeet.repositories.PlayerRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +16,12 @@ public class CommentController {
 
     private final CommentRepository commentDao;
     private final EventRepository eventsDao;
+    private final PlayerRepository playersDao;
 
-    public CommentController(CommentRepository commentDao, EventRepository eventsDao) {
+    public CommentController(CommentRepository commentDao, EventRepository eventsDao, PlayerRepository playersDao) {
         this.commentDao = commentDao;
         this.eventsDao = eventsDao;
+        this.playersDao = playersDao;
     }
 
     @GetMapping("/comments")
@@ -43,5 +46,16 @@ public class CommentController {
         comment.setEvent(eventsDao.getById(event.getId()));
         commentDao.save(newComment);
         return "redirect:/event/" + event.getId();
+    }
+
+    @GetMapping("/comment/{id}/delete")
+    public String deleteComment(@ModelAttribute("comment") Comment comment) {
+        Player currentPlayer = (Player) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Player current = playersDao.getById(currentPlayer.getId());
+        Comment currentComment = commentDao.getById(comment.getId());
+        if (playersDao.getById(currentComment.getPlayer().getId()).getId() == current.getId() || playersDao.getById(current.getId()).getId() == eventsDao.getById(currentComment.getEvent().getId()).getPlayer().getId()) {
+            commentDao.delete(commentDao.getById(currentComment.getId()));
+        }
+        return "redirect:/event/" + currentComment.getEvent().getId();
     }
 }
