1297e20c8dd5900cf8e6c060e07dbf218ee88397
Michael Friend <michael.l.friend2@gmail.com>
2022-08-29 14:21:31 -0500

    added intended redirects for events page

--- src/main/java/com/codeup/sportmeet/controllers/EventController.java
+++ src/main/java/com/codeup/sportmeet/controllers/EventController.java
@@ -10,6 +10,7 @@ import com.codeup.sportmeet.repositories.PlayerRepository;
 import com.codeup.sportmeet.repositories.SportRepository;
 import org.hibernate.type.LocalTimeType;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
@@ -59,15 +60,18 @@ public class EventController {
 
     @GetMapping("event/create")
     public String showCreateEvent(Model model) {
-        model.addAttribute("event", new Event());
-        model.addAttribute("sports", sportsDao.findAll());
-        return "event/create";
+        if (String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).equalsIgnoreCase("anonymousUser")) {
+            return "redirect:/login";
+        } else {
+            model.addAttribute("event", new Event());
+            model.addAttribute("sports", sportsDao.findAll());
+            return "event/create";
+        }
     }
 
     @PostMapping("event/create")
     public String createEvent(@ModelAttribute Event event) throws ParseException {
         Player currentPlayer = (Player) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         String today = format.format(new Date());
         Date todayAsDate = format.parse(today);
@@ -119,92 +123,112 @@ public class EventController {
 
     @GetMapping("event/{id}")
     public String showEvent(Model model, @PathVariable long id) throws ParseException {
-        Event event = eventsDao.getById(id);
-        List<String> eventPlayerUsernames = new ArrayList<>();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        String today = format.format(new Date());
-        Date todayAsDate = format.parse(today);
-        LocalTime now = LocalTime.now();
-        if (format.parse(event.getDate()).equals(todayAsDate) && Integer.parseInt(event.getStartTime().substring(0,2)) <= now.getHour() && Integer.parseInt(event.getStartTime().substring(3,5)) <= now.getMinute()) {
-            System.err.println(Integer.parseInt(event.getStartTime().substring(3,5)));
-            System.err.println(now.getMinute());
-            event.setStarted(true);
-            eventsDao.save(event);
-        }
-        for (Player player : eventsDao.getById(id).getPlayers()) {
-            eventPlayerUsernames.add(player.getUsername());
-        }
-        model.addAttribute("event", event);
-        model.addAttribute("comment", new Comment());
-        model.addAttribute("comments", eventsDao.getById(id).getComments());
-        model.addAttribute("eventPlayerUsernames", eventPlayerUsernames);
-        model.addAttribute("mbKey", mbKey);
-        model.addAttribute("wKey", wKey);
-        if (event.getLocation() == null) {
-            event.setLocation("San Antonio, TX");
-            model.addAttribute("geocodeLoc", event.getLocation());
+        if (String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).equalsIgnoreCase("anonymousUser")) {
+            return "redirect:/login";
         } else {
-            model.addAttribute("geocodeLoc", event.getLocation());
+
+            Player currentPlayer = (Player) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Event event = eventsDao.getById(id);
+            List<String> eventPlayerUsernames = new ArrayList<>();
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            String today = format.format(new Date());
+            Date todayAsDate = format.parse(today);
+            LocalTime now = LocalTime.now();
+            if (format.parse(event.getDate()).equals(todayAsDate) && Integer.parseInt(event.getStartTime().substring(0, 2)) <= now.getHour() && Integer.parseInt(event.getStartTime().substring(3, 5)) <= now.getMinute()) {
+                System.err.println(Integer.parseInt(event.getStartTime().substring(3, 5)));
+                System.err.println(now.getMinute());
+                event.setStarted(true);
+                eventsDao.save(event);
+            }
+            for (Player player : eventsDao.getById(id).getPlayers()) {
+                eventPlayerUsernames.add(player.getUsername());
+            }
+            model.addAttribute("event", event);
+            model.addAttribute("comment", new Comment());
+            model.addAttribute("comments", eventsDao.getById(id).getComments());
+            model.addAttribute("eventPlayerUsernames", eventPlayerUsernames);
+            model.addAttribute("mbKey", mbKey);
+            model.addAttribute("wKey", wKey);
+            if (event.getLocation() == null) {
+                event.setLocation("San Antonio, TX");
+                model.addAttribute("geocodeLoc", event.getLocation());
+            } else {
+                model.addAttribute("geocodeLoc", event.getLocation());
+            }
+            model.addAttribute("eventDate", event.getDate());
+            return "event/show";
         }
-        model.addAttribute("eventDate", event.getDate());
-        return "event/show";
     }
 
     @PostMapping("event/{id}/attend")
     public String attendEvent(@ModelAttribute("event") Event event, Model model) throws Exception {
-        Player currentPlayer = (Player) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Player pl = playersDao.getById(currentPlayer.getId());
-        Event ev = eventsDao.getById(event.getId());
-        if (playersDao.getById(currentPlayer.getId()).getAttendingEvents() == null) {
-            List<Event> events = new ArrayList<>();
-            events.add(event);
-            pl.setAttendingEvents(events);
-        }
-        else {
-            pl.getAttendingEvents().add(event);
-        }
-        playersDao.save(pl);
+        if (String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).equalsIgnoreCase("anonymousUser")) {
+            return "redirect:/login";
+        } else {
+            Player currentPlayer = (Player) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Player pl = playersDao.getById(currentPlayer.getId());
+            Event ev = eventsDao.getById(event.getId());
+            if (playersDao.getById(currentPlayer.getId()).getAttendingEvents() == null) {
+                List<Event> events = new ArrayList<>();
+                events.add(event);
+                pl.setAttendingEvents(events);
+            } else {
+                pl.getAttendingEvents().add(event);
+            }
+            playersDao.save(pl);
 
-        if (eventsDao.getById(event.getId()).getPlayers() == null) {
-            List<Player> players = new ArrayList<>();
-            players.add(currentPlayer);
-            ev.setPlayers(players);
-            ev.setPlayersAttending(eventsDao.getById(event.getId()).getPlayers().size());
+            if (eventsDao.getById(event.getId()).getPlayers() == null) {
+                List<Player> players = new ArrayList<>();
+                players.add(currentPlayer);
+                ev.setPlayers(players);
+                ev.setPlayersAttending(eventsDao.getById(event.getId()).getPlayers().size());
+            } else {
+                ev.getPlayers().add(currentPlayer);
+                ev.setPlayersAttending(eventsDao.getById(event.getId()).getPlayers().size());
+            }
+            eventsDao.save(ev);
+            System.out.println(currentPlayer.getEvents());
+            return "redirect:/event/" + ev.getId();
         }
-        else {
-            ev.getPlayers().add(currentPlayer);
-            ev.setPlayersAttending(eventsDao.getById(event.getId()).getPlayers().size());
-        }
-        eventsDao.save(ev);
-        System.out.println(currentPlayer.getEvents());
-        return "redirect:/event/" + ev.getId();
     }
 
     @GetMapping(value = "/event/{id}/delete")
     public String deleteEvent(@ModelAttribute("event") Event event) {
-        for (Comment comment : commentsDao.findAll()) {
-            long commentEventID = comment.getEvent().getId();
-            if (commentEventID == event.getId()) {
-                commentsDao.delete(comment);
+        if (String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).equalsIgnoreCase("anonymousUser")) {
+            return "redirect:/login";
+        } else {
+            for (Comment comment : commentsDao.findAll()) {
+                long commentEventID = comment.getEvent().getId();
+                if (commentEventID == event.getId()) {
+                    commentsDao.delete(comment);
+                }
             }
+            eventsDao.delete(event);
+            return "redirect:/events";
         }
-        eventsDao.delete(event);
-        return "redirect:/events";
     }
 
     @GetMapping(value = "/event/{id}/edit")
     public String showEditEvent(@PathVariable long id, Model model, HttpSession session) {
-        session.setAttribute("id", id);
-        model.addAttribute("sports", sportsDao.findAll());
-        model.addAttribute("event", eventsDao.getById(id));
-        return "event/edit";
+        if (String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).equalsIgnoreCase("anonymousUser")) {
+            return "redirect:/login";
+        } else {
+            session.setAttribute("id", id);
+            model.addAttribute("sports", sportsDao.findAll());
+            model.addAttribute("event", eventsDao.getById(id));
+            return "event/edit";
+        }
     }
 
     @PostMapping(value = "event/{id}/edit")
     public String editEvent(@ModelAttribute("event") Event event, HttpSession session) {
-        Long id = (Long) session.getAttribute("id");
-        eventsDao.updateEvent(id, event.getTitle(), event.getDescription(), event.getLocation(), event.getStartTime(), event.getDate(), event.getSport());
-        return "redirect:/events";
+        if (String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).equalsIgnoreCase("anonymousUser")) {
+            return "redirect:/login";
+        } else {
+            Long id = (Long) session.getAttribute("id");
+            eventsDao.updateEvent(id, event.getTitle(), event.getDescription(), event.getLocation(), event.getStartTime(), event.getDate(), event.getSport());
+            return "redirect:/events";
+        }
     }
 
     @GetMapping("event/search")
@@ -220,39 +244,55 @@ public class EventController {
 
     @GetMapping("/event/{id}/upload")
     public String viewAddProfilePhoto(@PathVariable long id, Model model, HttpSession session) {
-        session.setAttribute("id", id);
-        model.addAttribute("fsKey",fsKey);
-        model.addAttribute("event", eventsDao.getById(id));
-        return "event/upload";
+        if (String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).equalsIgnoreCase("anonymousUser")) {
+            return "redirect:/login";
+        } else {
+            session.setAttribute("id", id);
+            model.addAttribute("fsKey", fsKey);
+            model.addAttribute("event", eventsDao.getById(id));
+            return "event/upload";
+        }
     }
 
     @PostMapping("/event/{id}/upload")
     public String savePhoto(@RequestParam(name="profile_img") String img, @PathVariable long id){
-        Event event = eventsDao.getById(id);
-        event.setEventPicUrl(img);
-        eventsDao.save(event);
-        return "redirect:/event/" + id;
+        if (String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).equalsIgnoreCase("anonymousUser")) {
+            return "redirect:/login";
+        } else {
+            Event event = eventsDao.getById(id);
+            event.setEventPicUrl(img);
+            eventsDao.save(event);
+            return "redirect:/event/" + id;
+        }
     }
 
     @GetMapping("/event/{id}/start")
     public String showStart(@PathVariable long id, Model model) {
-        Event event = eventsDao.getById(id);
-        model.addAttribute("event", event);
-        model.addAttribute("players", event.getPlayers());
-        return "event/start";
+        if (String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).equalsIgnoreCase("anonymousUser")) {
+            return "redirect:/login";
+        } else {
+            Event event = eventsDao.getById(id);
+            model.addAttribute("event", event);
+            model.addAttribute("players", event.getPlayers());
+            return "event/start";
+        }
     }
 
     @PostMapping("/event/{id}/start")
     public String submitStart(@ModelAttribute("event") Event event, @ModelAttribute("players") ArrayList<Player> players, @RequestParam("confirmedPlayers") List<Long> confirmedPlayers) {
-        Event event1 = eventsDao.getById(event.getId());
-        event1.setConfirmed(true);
-        List<Player> newPlayers = new ArrayList<>();
-        for (Long id : confirmedPlayers) {
-            newPlayers.add(playersDao.getById(id));
+        if (String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).equalsIgnoreCase("anonymousUser")) {
+            return "redirect:/login";
+        } else {
+            Event event1 = eventsDao.getById(event.getId());
+            event1.setConfirmed(true);
+            List<Player> newPlayers = new ArrayList<>();
+            for (Long id : confirmedPlayers) {
+                newPlayers.add(playersDao.getById(id));
+            }
+            event1.setPlayers(newPlayers);
+            event1.setPlayersAttending(eventsDao.getById(event1.getId()).getPlayers().size());
+            eventsDao.save(event1);
+            return "redirect:/event/" + event1.getId();
         }
-        event1.setPlayers(newPlayers);
-        event1.setPlayersAttending(eventsDao.getById(event1.getId()).getPlayers().size());
-        eventsDao.save(event1);
-        return "redirect:/event/" + event1.getId();
     }
 }
